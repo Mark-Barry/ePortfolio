13ba92d318a573724cdd0e6188736bc0756ecdb8
bodbDerg <markbarryty@hotmail.com>
2019-07-19 12:44:24 +0100

    Last of the comments

--- Scripts/canvasScripts.js
+++ Scripts/canvasScripts.js
@@ -1,11 +1,17 @@
-// 
-// 
-// 
+// This pages contains all the javascript for the canvas application to work 
+// on the DesignACover.html page.
+// Basically it controls all the heavy lifting
 
+//First of all set up some variables to use
+//This gets the canvas based on the canvas's id set in the html
 var canvas = document.getElementById("coverCanvas");
+//This sets the type of canvas it is, 2D
 var ctx = canvas.getContext("2d");
+//This gets the area to paint to, the same as the canvas.
 var painting = document.getElementById("coverCanvas");
+//This sets up the area to paint on
 var paintStyle = window.getComputedStyle(painting);
+//This is a place holder for a changing variable for an image
 var image;
 canvas.width = parseInt(paintStyle.getPropertyValue("width"));
 canvas.height = parseInt(paintStyle.getPropertyValue("height"));
